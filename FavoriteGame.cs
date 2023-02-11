5f0934a90108e220a50ffcdeda009946a399b118
James Vavrik <jamesvavrikjr@gmail.com>
2023-02-10 18:33:54 -0600

    adding azure cosmos connection

--- FavoriteGame.cs
+++ FavoriteGame.cs
@@ -15,20 +15,41 @@ namespace Jvavrik.Functions
         }
 
         [Function("FavoriteGame")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        public async Task<MultiResponse> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var body = await new StreamReader(req.Body).ReadToEndAsync();
             _logger.LogInformation(body);
-
-
+            
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
             response.WriteString($"Thank you for your submission! {body}");
-
-            return response;
+            return new MultiResponse()
+                {
+                    FavoriteGame = new FavoriteGameModel
+                    {
+                        Name = body,
+                        id = Guid.NewGuid().ToString(),
+                    },
+                    HttpResponse = response
+                };
         }
+    }
+
+    public class MultiResponse{
+        [CosmosDBOutput(
+            databaseName: "FavoriteGame",
+            containerName:"Games", 
+            Connection = "CosmosConnection", 
+            CreateIfNotExists =true)]
+            public FavoriteGameModel FavoriteGame {get;set;}
+            public HttpResponseData HttpResponse { get; set; }
+    }
+
+    public class FavoriteGameModel{
+        public string Name {get;set;}
+        public string id {get;set;}
     }
 }
